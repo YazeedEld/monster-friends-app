07ff7584fa783f6729c6fb524824c826c49ed77b
Yazeed Eldigair <74137136+YazeedEld@users.noreply.github.com>
2021-12-20 16:12:50 +0400

    Add files via upload

---
+++ src/containers/App.js
@@ -0,0 +1,55 @@
+import React from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+// import { robots } from "./robots";
+import Scroll from "../components/Scroll";
+import "./App.css";
+
+class App extends React.Component {
+  constructor() {
+    // Lifecycle Methods
+    super();
+    this.state = {
+      robots: [],
+      searchfiled: "",
+    };
+  }
+
+  componentDidMount() {
+    // Lifecycle Methods
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        this.setState({ robots: users });
+      });
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfiled: event.target.value });
+  };
+
+  render() {
+    const {robots, searchfiled} = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchfiled.toLowerCase());
+    });
+
+    if (robots.length == 0) {
+      return <h1 className="tc f1">Loading...</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+    }
+  }
+}
+
+export default App;
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: Math.floor(Math.random() * 1000),
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@melissa.tv",
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net",
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    name: "Patricia Lebsack",
+    username: "Karianne",
+    email: "Julianne.OConner@kory.org",
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    name: "Chelsey Dietrich",
+    username: "Kamren",
+    email: "Lucio_Hettinger@annie.ca",
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    name: "Mrs. Dennis Schulist",
+    username: "Leopoldo_Corkery",
+    email: "Karley_Dach@jasper.info",
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    name: "Kurtis Weissnat",
+    username: "Elwyn.Skiles",
+    email: "Telly.Hoeger@billy.biz",
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    name: "Nicholas Runolfsdottir V",
+    username: "Maxime_Nienow",
+    email: "Sherwood@rosamond.me",
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    name: "Glenna Reichert",
+    username: "Delphine",
+    email: "Chaim_McDermott@dana.io",
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    name: "Clementina DuBuque",
+    username: "Moriah.Stanton",
+    email: "Rey.Padberg@karina.biz",
+  },
+];
