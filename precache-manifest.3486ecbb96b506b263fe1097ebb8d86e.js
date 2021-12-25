e7b1e0a7b6073d21f920ace5921e1ca268d97c57
YazeedEld <yazeed_eldigair@yahoo.com>
2021-12-26 02:11:06 +0400

    Updates

---
+++ precache-manifest.3486ecbb96b506b263fe1097ebb8d86e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d711f6372dbf2cfc9bef",
+    "url": "/Monster-Friends-App/static/css/main.c1e7cf86.chunk.css"
+  },
+  {
+    "revision": "d711f6372dbf2cfc9bef",
+    "url": "/Monster-Friends-App/static/js/main.cb036620.chunk.js"
+  },
+  {
+    "revision": "84b745b95e184ca6479d",
+    "url": "/Monster-Friends-App/static/js/runtime~main.3370c5a6.js"
+  },
+  {
+    "revision": "15a45fe39e6c26390bb2",
+    "url": "/Monster-Friends-App/static/css/2.ea91ba91.chunk.css"
+  },
+  {
+    "revision": "15a45fe39e6c26390bb2",
+    "url": "/Monster-Friends-App/static/js/2.f2c8162a.chunk.js"
+  },
+  {
+    "revision": "f2c121df5bc05f03f3ea",
+    "url": "/Monster-Friends-App/static/js/3.b1248334.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Monster-Friends-App/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "bd57cf8fd491c8b527a1a98c236d5998",
+    "url": "/Monster-Friends-App/index.html"
+  }
+];
